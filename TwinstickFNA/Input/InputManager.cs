c72baa06af109c78eaceb66810208e7314ab19d0
Raoul1808 <37770353+Raoul1808@users.noreply.github.com>
2023-01-17 23:03:56 +0100

    Mouse Input

--- TwinstickFNA/Input/InputManager.cs
+++ TwinstickFNA/Input/InputManager.cs
@@ -5,19 +5,28 @@ namespace TwinstickFNA.Input
 {
     public static class InputManager
     {
+        private static MouseState _oldMouse, _mouse;
         private static GamePadState _oldGamePad, _gamePad;
 
         static InputManager()
         {
+            _oldMouse = default;
+            _mouse = default;
             _oldGamePad = default;
             _gamePad = default;
         }
 
         public static void Update()
         {
+            _oldMouse = _mouse;
             _oldGamePad = _gamePad;
+            _mouse = Mouse.GetState();
             _gamePad = GamePad.GetState(PlayerIndex.One);
         }
+
+        public static bool GetMouseLeft() => _mouse.LeftButton == ButtonState.Pressed;
+        public static bool GetMouseLeftClick() => _mouse.LeftButton == ButtonState.Pressed && _oldMouse.LeftButton == ButtonState.Released;
+        public static Point GetMousePos() => new Point(_mouse.X, _mouse.Y);
 
         public static bool GetButton(Buttons button) => _gamePad.IsButtonDown(button);
         public static bool GetButtonPress(Buttons button) => _gamePad.IsButtonDown(button) && _oldGamePad.IsButtonUp(button);
