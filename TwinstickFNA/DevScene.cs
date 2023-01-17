9d07fd72b4b144c35b561efef021cc95e267e7be
Raoul1808 <37770353+Raoul1808@users.noreply.github.com>
2023-01-17 21:49:09 +0100

    Some "platformer" physics

--- TwinstickFNA/DevScene.cs
+++ TwinstickFNA/DevScene.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using TwinstickFNA.Input;
 
 namespace TwinstickFNA
@@ -7,18 +8,24 @@ namespace TwinstickFNA
     public class DevScene
     {
         private Vector2 _position;
+        private Vector2 _velocity;
         private const int PLAYER_SQUARE_SCALE = 100;
 
         public DevScene()
         {
             _position = new Vector2(100, 100);
+            _velocity = new Vector2(0);
         }
         
         public void Update()
         {
-            var vel = InputManager.GetLeftStick() * 2;
-            vel.Y *= -1;
-            _position += vel;
+            _velocity.X = InputManager.GetLeftStick().X * 5f;
+            _velocity.Y += 1f;
+            if (_velocity.Y >= 10f)
+                _velocity.Y = 10f;
+            if (InputManager.GetButtonPress(Buttons.A))
+                _velocity.Y = -20f;
+            _position += _velocity;
         }
 
         public void Draw(SpriteBatch spriteBatch)
